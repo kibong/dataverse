d4a8b59758c7fb1f01d9931214b60b8fa593297e
kibong <kibong.choi@moreh.io>
2024-04-01 07:17:21 +0000

    add jsonl load & save

--- dataverse/etl/data_ingestion/jsonl.py
+++ dataverse/etl/data_ingestion/jsonl.py
@@ -2,14 +2,14 @@
 Copyright (c) 2024-present Upstage Co., Ltd.
 Apache-2.0 license
 """
-
 from typing import List, Union
+from pyspark.sql import SparkSession
 from pyspark.rdd import RDD
 from dataverse.etl import register_etl
 
 @register_etl
 def data_ingestion___jsonl___jsonl2raw(
-    spark, path: Union[str, List[str]], repartition=20, _args=None, **kwargs
+    spark: SparkSession, path: Union[str, List[str]], repartition=20, *args, **kwargs
 ) -> RDD:
     """
     Reads JSONL files into an RDD and repartitions it.
@@ -18,7 +18,6 @@ def data_ingestion___jsonl___jsonl2raw(
         spark (SparkSession): The Spark session.
         path (str or list): The path of the JSONL files.
         repartition (int): The number of partitions.
-        _args (optional): Additional arguments. Defaults to None.
 
     Returns:
         rdd: The repartitioned RDD containing the data from the JSONL files.
@@ -26,8 +25,43 @@ def data_ingestion___jsonl___jsonl2raw(
     if isinstance(path, str):
         path = [path]
 
-    rdd = spark.sparkContext.textFile(",".join(path))
-    rdd = rdd.repartition(repartition)
-    rdd = rdd.map(lambda line: eval(line))
-
+    df = spark.read.json(path)
+    rdd = df.rdd.repartition(repartition)
+    rdd = rdd.map(lambda row: row.asDict())
     return rdd
+
+
+
+# """
+# Copyright (c) 2024-present Upstage Co., Ltd.
+# Apache-2.0 license
+# """
+
+# from typing import List, Union
+# from pyspark.rdd import RDD
+# from dataverse.etl import register_etl
+
+# @register_etl
+# def data_ingestion___jsonl___jsonl2raw(
+#     spark, path: Union[str, List[str]], repartition=20, _args=None, **kwargs
+# ) -> RDD:
+#     """
+#     Reads JSONL files into an RDD and repartitions it.
+
+#     Args:
+#         spark (SparkSession): The Spark session.
+#         path (str or list): The path of the JSONL files.
+#         repartition (int): The number of partitions.
+#         _args (optional): Additional arguments. Defaults to None.
+
+#     Returns:
+#         rdd: The repartitioned RDD containing the data from the JSONL files.
+#     """
+#     if isinstance(path, str):
+#         path = [path]
+
+#     rdd = spark.sparkContext.textFile(",".join(path))
+#     rdd = rdd.repartition(repartition)
+#     rdd = rdd.map(lambda line: eval(line))
+
+#     return rdd
